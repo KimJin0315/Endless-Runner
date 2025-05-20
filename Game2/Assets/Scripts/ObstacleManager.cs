25b76958414566a1b4e00b7eab0c08a42f81c996
KimJin0315 <xnxl159@naver.com>
2025-05-21 00:14:45 +0900

    장애물 활성화 시스템

--- Game2/Assets/Scripts/ObstacleManager.cs
+++ Game2/Assets/Scripts/ObstacleManager.cs
@@ -1,39 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    [SerializeField] int random;
 
-    [SerializeField] GameObject [] obstacles;
+    [SerializeField] int createCount = 5;
 
-    [SerializeField] Transform parentPostion;
+    [SerializeField] List<GameObject> obstacles;
 
-    [SerializeField] List<GameObject> obstaclesList;
+    [SerializeField] GameObject [] prefab;
 
+    [SerializeField] Transform [] transforms; 
 
     void Start()
     {
+        Create();
 
-        for(int i = 0; i < obstacles.Length; i++)
+        StartCoroutine(ActiveObstacle());
+    }
+
+    public void Create()
+    {
+        for (int i = 0; i < createCount; i++)
         {
-            GameObject clone = Instantiate(obstacles[i], parentPostion);
+            GameObject clone = Instantiate(prefab[Random.Range(0, prefab.Length)], transform);
 
-            clone.gameObject.SetActive(false);
+            clone.SetActive(false);
 
-            obstaclesList.Add(clone);
+            obstacles.Add(clone);
         }
 
-
     }
 
-    void Update()
+    public IEnumerator ActiveObstacle()
     {
-        RandomCreate();
+        while(true)
+        {
+            random = Random.Range(0, obstacles.Count);
+
+            // 현재 게임 오브젝트가 활성화되어 있는 지 확인합니다.
+            while (obstacles[random].activeSelf == true)
+            {
+                // 현재 인덱스에 있는 게임 오브젝트가 활성화되어 있으면
+                // random 변수의 값을 +1을 해서 다시 검색합니다.
+                random = (random + 1) % obstacles.Count;
+            }
+
+            obstacles[random].transform.position = transforms[Random.Range(0, transforms.Length)].position;
+
+            obstacles[random].SetActive(true);
+
+            yield return new WaitForSeconds(5);
+        }
     }
 
-    void RandomCreate()
-    {
 
-    }
 }
