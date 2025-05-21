e9e54550ff1b79500587aaa372acd09e86393c7c
KimJin0315 <xnxl159@naver.com>
2025-05-21 19:44:49 +0900

    (2025.05.21) 마우스 관리자 시스템

---
+++ Game2/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour, Collidable
+{
+    [SerializeField] float speed;
+
+    public void Activate()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        
+    }
+
+
+}
